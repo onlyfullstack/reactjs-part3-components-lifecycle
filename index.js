241fc1a4b387eb46f01d6085a668247094161807
onlyfullstack <onlyfullstackdev@gmail.com>
2019-11-12 10:07:59 +0530

    1 file modified

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ class App extends Component {
       <div>
         <button onClick={this.handleClick} >Increment Notification</button>
         <Notification notification={this.state.notification} />
+        <a
+          href="https://onlyfullstack.blogspot.com/2019/11/react-tutorial.html"
+          target="_blank">
+          ReactJS Tutorial on Only Fullstack
+        </a>
+        <a
+          href="https://onlyfullstack.blogspot.com/2019/11/react-tutorial.html"
+          target="_blank">
+          <img
+            src="https://1.bp.blogspot.com/-I276A9NSgjY/Xck5TadIjpI/AAAAAAAAAi0/gFb-9achXtk4XkELxVyRNR3UXuMJGsUfQCLcBGAsYHQ/s640/React%2BTutorial.jpg" height="30%" width="90%"/>
+        </a>
       </div>
     );
   }
